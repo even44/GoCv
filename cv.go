11e234eb9758c635fdfd323e1ee0df9330b97df0
Even <even.andreas.ingeberg@ebnett.no>
2024-10-10 14:19:14 +0200

    init

---
+++ cv.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+        "net/http"
+        "fmt"
+)
+
+var port = 3000
+
+func main() {
+        fmt.Printf("Hosting CV listening at port %d", port)
+        http.Handle("/", http.FileServer(http.Dir("./public")))
+        http.ListenAndServe(":3000", nil)
+        
+        
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module even44/cv
+
+go 1.22.2
