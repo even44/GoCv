40405abf413be947402b611c39990bb09835557e
Even <even.andreas.ingeberg@ebnett.no>
2024-10-11 00:39:32 +0200

    Update cv.go

--- cv.go
+++ cv.go
@@ -10,7 +10,7 @@ var port = 3000
 func main() {
         fmt.Printf("Hosting CV listening at port %d", port)
         http.Handle("/", http.FileServer(http.Dir("./public")))
-        http.ListenAndServe(":3000", nil)
+        http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
         
         
 }
